a93f6cb08c4cdb4401df2f44ba5191f536837ace
Victor Lam <mingmanhk@gmail.com>
2021-07-25 01:15:16 -0700

    bug fix

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ var GetTodayWeather = function (city) {
       }
     })
     .catch(function (error) {
-      showalertmsg('Unable to connect to GitHub');
+      showalertmsg(error);
     });
 };
 
@@ -60,7 +60,7 @@ var Get5daysWeather = function (city) {
       }
     })
     .catch(function (error) {
-      showalertmsg('Unable to connect to GitHub');
+      showalertmsg(error);
     });
 };
 
@@ -79,7 +79,7 @@ var GetTodayUV = function (city, lat, lon, dt) {
       }
     })
     .catch(function (error) {
-      showalertmsg('Unable to connect to GitHub');
+      showalertmsg(error);
     });
 };
 
